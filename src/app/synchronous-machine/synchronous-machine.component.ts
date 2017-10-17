d51c1b1dd902765a92f6d47dde8a1a5458b07c5a
Cruvis <cruvinel.rafael@gmail.com>
2017-10-17 19:59:43 -0200

    Create graph torch vs Rpm MI

--- src/app/synchronous-machine/synchronous-machine.component.ts
+++ src/app/synchronous-machine/synchronous-machine.component.ts
@@ -19,11 +19,75 @@ export class SynchronousMachineComponent implements OnInit {
   //functions
   graficalizer;
   changeRange;
+  p;
+  f;
+  r1;
+  r2l;
+  rth;
+  s;
+  t;
+  v1;
+  vth;
+  x1;
+  xth;
+  x2l;
+  xm;
+  i2l;
+  ws;
+
+
+
+
 
     constructor() {
       function changeRange() {
         this.data = graficalizer(this.value, this.percentageRtwo);
         console.log("new value: " + this.value);
+      }
+
+      function calcVth(x1, xm, v1, r1) {
+        let num = xm * v1;
+        let den = Math.sqrt(r1 * r1 + (x1 + xm) * (x1 + xm));
+        // if(true){
+        //   console.log("Calculando Vth")
+        //   console.log("x1 = ", x1, "xm = ", xm, "v1 = ", v1, "r1 = ", r1)
+        //   console.log("num = ", num,"den = ", den, "return", num/den);
+        // }
+        return num/den;
+      }
+
+      function calcRth(x1, xm, v1, r1) {
+        let num = xm * xm * r1;
+        let den = r1 * r1 + (x1 + xm) * (x1 + xm);
+        // if(true){
+        //   console.log("Calculando Rth")
+        //   console.log("x1 = ", x1, "xm = ", xm, "v1 = ", v1, "r1 = ", r1)
+        //   console.log("num = ", num,"den = ", den, "return", num/den);
+        // }
+        return num/den;
+      }
+
+      function calcXth(x1, xm, v1, r1) {
+        let num = xm * (xm * x1 + x1 * x1 + r1 * r1);
+        let den = r1 * r1 + (x1 + xm) * (x1 + xm);
+        // if(true){
+        //   console.log("Calculando Xth")
+        //   console.log("x1 = ", x1, "xm = ", xm, "v1 = ", v1, "r1 = ", r1)
+        //   console.log("num = ", num,"den = ", den, "return", num/den);
+        // }
+        return num/den;
+      }
+
+      function calcNs(f, p){
+        return 120 * f / p;
+      }
+
+      function calcWs(ns){
+        return 2 * Math.PI * ns / 60;
+      }
+
+      function calcS(ns, n){
+        return (ns - n ) / ns;
       }
 
       function vth(Xone, Xm, Vone, Rone) {
@@ -36,6 +100,39 @@ export class SynchronousMachineComponent implements OnInit {
 
       function xth(Xone, Xm, Vone, Rone) {
         return (Xm * (Xm * Xone + Xone * Xone + Rone * Rone)) / (Rone * Rone + (Xone + Xm) * (Xone + Xm));
+      }
+
+      function calcTorque(n){
+        let p, f, ns, w, xm, x1, r1, r2l, x2l, ws, v1, rth, xth, vth, s;
+        let num, den;
+        p = 6;
+        f = 60;
+        ns = calcNs(f, p);
+        ws = calcWs(ns);
+        xm = 273.04;
+        x1 = 7.96;
+        r1 = 2.8;
+        r2l = 2.12;
+        x2l = 7.96;
+        v1 = 2200 / Math.sqrt(3);
+        rth = calcRth(x1, xm, v1, r1);
+        xth = calcXth(x1, xm, v1, r1);
+        vth = calcVth(x1, xm, v1, r1);
+
+        //torque
+        s = calcS(ns, n);
+
+        num = vth* vth * r2l;
+        den = s * ws * ( ( rth + r2l / s) * (rth + r2l / s) + ( xth + x2l ) * ( xth + x2l ) );
+
+        console.log(num/den);
+
+        return num / den;
+        // num = 3 * Vth * Vth * Rtwo;
+        // div = ws * s * (Math.pow((Rth + Rtwo / s), 2) + Math.pow((Xth + Xtwo), 2));
+
+        //T(i,j)=(1/wsyn) * Vth(j)^2 / ( (Rth+R2l/s(i,j)) ^2 + (Xth+X2l)^2 )*(R2l/s(i,j));
+        //T(i,j)=(1/wsyn) * Vth(j)^2 / ( (Rth+R2l/s(i,j)) ^2 + (Xth+X2l)^2 )*(R2l/s);
       }
 
       function torque(i, percentVoltage, percentRtwo) {
@@ -68,12 +165,12 @@ export class SynchronousMachineComponent implements OnInit {
           funct5 = [];
 
         //Data is represented as an array of {x,y} pairs.
-        for (var i = 0; i <= 100; i++) {
-          funct1.push({ x: i * 12, y: torque(i, 100, 100) });
-          funct2.push({ x: i * 12, y: torque(i, 75, 100) });
-          funct3.push({ x: i * 12, y: torque(i, 50, 100) });
-          funct4.push({ x: i * 12, y: torque(i, 25, 100) });
-          funct5.push({ x: i * 12, y: torque(i, valueOne, valueTwo) });
+        for (var i = 0; i < 100; i++) {
+           funct1.push({ x: i * 12, y: calcTorque(i*12) });
+          //  funct2.push({ x: i * 12, y: torque(i, 75, 100) });
+          //  funct3.push({ x: i * 12, y: torque(i, 50, 100) });
+          //  funct4.push({ x: i * 12, y: torque(i, 25, 100) });
+          //  funct5.push({ x: i * 12, y: torque(i, valueOne, valueTwo) });
         }
 
         //Line chart data should be sent as an array of series objects.
@@ -83,27 +180,27 @@ export class SynchronousMachineComponent implements OnInit {
             key: 'Vth = 100%', //key  - the name of the series.
             color: '#ff7f0e'  //color - optional: choose your own line color.
           },
-          {
-            values: funct2,
-            key: 'Vth = 75%',
-            color: '#2ca02c'
-          },
-          {
-            values: funct3,
-            key: 'Vth = 50%',
-            color: '#7777ff',
-            //area: true
-          },
-          {
-            values: funct4, //values - represents the array of {x,y} data points
-            key: 'Vth = 25%', //key  - the name of the series.
-            color: '#0f0'  //color - optional: choose your own line color.
-          },
-          {
-            values: funct5, //values - represents the array of {x,y} data points
-            key: 'Vth = ' + valueOne + '%', //key  - the name of the series.
-            color: '#ff1200'  //color - optional: choose your own line color.
-          }
+          // {
+          //   values: funct2,
+          //   key: 'Vth = 75%',
+          //   color: '#2ca02c'
+          // },
+          // {
+          //   values: funct3,
+          //   key: 'Vth = 50%',
+          //   color: '#7777ff',
+          //   //area: true
+          // },
+          // {
+          //   values: funct4, //values - represents the array of {x,y} data points
+          //   key: 'Vth = 25%', //key  - the name of the series.
+          //   color: '#0f0'  //color - optional: choose your own line color.
+          // },
+          // {
+          //   values: funct5, //values - represents the array of {x,y} data points
+          //   key: 'Vth = ' + valueOne + '%', //key  - the name of the series.
+          //   color: '#ff1200'  //color - optional: choose your own line color.
+          // }
         ];
       };
 
@@ -135,10 +232,10 @@ export class SynchronousMachineComponent implements OnInit {
             tooltipHide: function (e) { console.log("tooltipHide"); }
           },
           xAxis: {
-            axisLabel: 'Torque'
+            axisLabel: 'T [Nm]'
           },
           yAxis: {
-            axisLabel: 'Velocidade Angular',
+            axisLabel: 'n [rpm]',
             tickFormat: function (d) {
               return d3.format('.02f')(d);
             },
