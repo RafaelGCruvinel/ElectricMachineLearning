9d300c775e2412f3600690dbe670f01bfb984738
Cruvis <cruvinel.rafael@gmail.com>
2017-10-19 21:42:40 -0200

    add graph FP x velocity - I

--- src/app/induction-machine/induction-machine.component.ts
+++ src/app/induction-machine/induction-machine.component.ts
@@ -20,13 +20,16 @@ export class InductionMachineComponent implements OnInit {
 
   options;
   options2;
+  options3;
   data;
   data2;
+  data3;
   percentageVoltage = 27;
   percentageRtwo = 100;
   // functions
   graficalizer;
   graficalizer2;
+  graficalizer3;
   changeRange;
   p;
   f;
@@ -130,7 +133,7 @@ export class InductionMachineComponent implements OnInit {
       }
 
       // calc corrente slide 15
-      function calcCorrente(n){
+      function calcCorrente(n, pv1, pr2){
         let p, f, ns, w, xm, x1, r1, r2l, x2l, ws, v1, rth, xth, vth, s;
         let num, den, j, z1, i1;
         let num1, den1;
@@ -141,9 +144,9 @@ export class InductionMachineComponent implements OnInit {
         xm = 273.04;
         x1 = 7.96;
         r1 = 2.8;
-        r2l = 2.12;
+        r2l = 2.12 * pr2;
         x2l = 7.96;
-        v1 = 2200 / Math.sqrt(3);
+        v1 = 2200 * pv1 / Math.sqrt(3);
         rth = calcRth(x1, xm, v1, r1);
         xth = calcXth(x1, xm, v1, r1);
         vth = calcVth(x1, xm, v1, r1);
@@ -156,14 +159,47 @@ export class InductionMachineComponent implements OnInit {
         den1 = math.complex(r2l/s, xm + x2l)
         z1 = math.add(z1, math.divide(num1, den1));
         i1 = math.divide(v1, z1)
-        console.log("\n", math.abs(i1))
-        return math.abs(i1);
+        return i1.abs();
 
         //Z1=R1+j*X1+(j*Xm*(R2l/s(i)+j*X2l))/(j*Xm+R2l/s(i)+j*X2l);
         //I1_cplx=V1/Z1;
         //I1(i)=abs(I1_cplx);
         //FP(i)=cos(angle(I1_cplx));
+      }
 
+      // calc corrente slide 15
+      function calcFatPot(n, pv1, pr2){
+        let p, f, ns, w, xm, x1, r1, r2l, x2l, ws, v1, rth, xth, vth, s;
+        let num, den, j, z1, i1;
+        let num1, den1;
+        p = 6;
+        f = 60;
+        ns = calcNs(f, p);
+        ws = calcWs(ns);
+        xm = 273.04;
+        x1 = 7.96;
+        r1 = 2.8;
+        r2l = 2.12 * pr2;
+        x2l = 7.96;
+        v1 = 2200 * pv1 / Math.sqrt(3);
+        rth = calcRth(x1, xm, v1, r1);
+        xth = calcXth(x1, xm, v1, r1);
+        vth = calcVth(x1, xm, v1, r1);
+
+        s = calcS(ns, n);
+
+        // Corrente
+        z1 = math.complex(r1, x1)
+        num1 = math.multiply(math.complex(r2l/s, x2l), math.complex(0, xm));
+        den1 = math.complex(r2l/s, xm + x2l)
+        z1 = math.add(z1, math.divide(num1, den1));
+        i1 = math.divide(v1, z1)
+        return math.cos(i1.arg());
+
+        //Z1=R1+j*X1+(j*Xm*(R2l/s(i)+j*X2l))/(j*Xm+R2l/s(i)+j*X2l);
+        //I1_cplx=V1/Z1;
+        //I1(i)=abs(I1_cplx);
+        //FP(i)=cos(angle(I1_cplx));
       }
 
 
@@ -175,7 +211,7 @@ export class InductionMachineComponent implements OnInit {
           funct5 = [];
 
         // Data is represented as an array of {x,y} pairs.
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i <= 100; i++) {
            funct1.push({ x: i * 12, y: calcTorque(i * 12, 1, 1) });
            funct2.push({ x: i * 12, y: calcTorque(i * 12, 0.75, 1) });
            funct3.push({ x: i * 12, y: calcTorque(i * 12, 1, 0.75) });
@@ -214,13 +250,15 @@ export class InductionMachineComponent implements OnInit {
         ];
       }
 
-      /*Create graph 01 */
+      /*Create graph 02 */
       function graficalizer2(valueOne, valueTwo) {
-        let funct1 = [];
+        let funct1 = [], funct2 = [], funct3 = [];
 
         // Data is represented as an array of {x,y} pairs.
-        for (let i = 0; i < 100; i++) {
-           funct1.push({ x: i * 12, y: calcCorrente(i * 12) });
+        for (let i = 0; i <= 100; i++) {
+           funct1.push({ x: i * 12, y: calcCorrente(i * 12, 1, 1) });
+           funct2.push({ x: i * 12, y: calcCorrente(i * 12, 0.75, 1) });
+           funct3.push({ x: i * 12, y: calcCorrente(i * 12, 1, 0.75) });
         }
 
         // Line chart data should be sent as an array of series objects.
@@ -229,16 +267,61 @@ export class InductionMachineComponent implements OnInit {
             values: funct1,  // values - represents the array of {x,y} data points
             key: 'Padrão', // key  - the name of the series.
             color: '#ff7f0e'  // color - optional: choose your own line color.
-          }
+          },
+          {
+            values: funct2,
+            key: 'Vth = 75%',
+            color: '#2ca02c'
+          },
+          {
+            values: funct3,
+            key: 'R2\'= 75%',
+            color: '#7777ff',
+            // area: true
+          },
         ];
       }
 
+      /*Create graph 03 */
+      function graficalizer3(valueOne, valueTwo) {
+        let funct1 = [], funct2 = [], funct3 = [];
+
+        // Data is represented as an array of {x,y} pairs.
+        for (let i = 0; i <= 100; i++) {
+           funct1.push({ x: i * 12, y: calcFatPot(i * 12, 1, 1) });
+           funct2.push({ x: i * 12, y: calcFatPot(i * 12, 0.75, 1) });
+           funct3.push({ x: i * 12, y: calcFatPot(i * 12, 1, 0.75) });
+        }
+
+        // Line chart data should be sent as an array of series objects.
+        return [
+          {
+            values: funct1,  // values - represents the array of {x,y} data points
+            key: 'Padrão', // key  - the name of the series.
+            color: '#ff7f0e'  // color - optional: choose your own line color.
+          },
+          {
+            values: funct2,
+            key: 'Vth = 75%',
+            color: '#2ca02c'
+          },
+          {
+            values: funct3,
+            key: 'R2\'= 75%',
+            color: '#7777ff',
+            // area: true
+          },
+        ];
+      }
 
       this.graficalizer = graficalizer;
       this.changeRange = changeRange;
 
       this.graficalizer2 = graficalizer2;
       //this.changeRange2 = changeRange2;
+
+      this.graficalizer3 = graficalizer3;
+      //this.changeRange3 = changeRange3;
     }
 
 
@@ -358,8 +441,64 @@ export class InductionMachineComponent implements OnInit {
       }
     };
 
-  this.data2 = this.graficalizer2(this.percentageVoltage, this.percentageRtwo);
+    this.data2 = this.graficalizer2(this.percentageVoltage, this.percentageRtwo);
 
+    this.options3 = {
+      chart: {
+        type: 'lineChart',
+        height: 450,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 40,
+          left: 55
+        },
+        x: function (d) { return d.x; },
+        y: function (d) { return d.y; },
+        useInteractiveGuideline: true,
+        dispatch: {
+          stateChange: function (e) { console.log('stateChange'); },
+          changeState: function (e) { console.log('changeState'); },
+          tooltipShow: function (e) { console.log('tooltipShow'); },
+          tooltipHide: function (e) { console.log('tooltipHide'); }
+        },
+        xAxis: {
+          axisLabel: 'n [rpm]'
+        },
+        yAxis: {
+          axisLabel: 'FP',
+          tickFormat: function (d) {
+            return d3.format('.02f')(d);
+          },
+          axisLabelDistance: -10
+        },
+        callback: function (chart) {
+          console.log('!!! lineChart callback !!!');
+        }
+      },
+      title: {
+        enable: true,
+        text: 'Curva Fator de Potência x Velocidade'
+      },
+      subtitle: {
+        enable: false,
+        text: 'Subtitle for simple line chart.',
+        css: {
+          'text-align': 'center',
+          'margin': '10px 13px 0px 7px'
+        }
+      },
+      caption: {
+        enable: true,
+        html: '<b>Figure 1.</b> Gráfico baseado no exemplo 5.2 do P. C. Sen, terceira edição.',
+        css: {
+          'text-align': 'justify',
+          'margin': '10px 13px 0px 7px'
+        }
+      }
+    };
+
+    this.data3 = this.graficalizer3(this.percentageVoltage, this.percentageRtwo);
 
   }
 }
