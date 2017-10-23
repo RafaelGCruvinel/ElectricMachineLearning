146ff7376683622611966ac0acc09de63d371045
Cruvis <cruvinel.rafael@gmail.com>
2017-10-22 23:09:41 -0200

    small text changes and layout

--- src/app/continuous-chain-machine/continuous-chain-machine.component.ts
+++ src/app/continuous-chain-machine/continuous-chain-machine.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-continuous-machine',
-    templateUrl: 'continuous-chain-machine.component.html'
+    templateUrl: 'continuous-chain-machine.component.html',
+    styleUrls: ['continuous-chain-machine.component.scss']
 })
 export class ContinuousChainMachineComponent implements OnInit {
 
