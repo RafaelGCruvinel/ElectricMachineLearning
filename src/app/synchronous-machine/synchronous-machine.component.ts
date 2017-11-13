7056fc063faa8006b2b8b6672052c0f10b5c007b
Cruvis <cruvinel.rafael@gmail.com>
2017-11-12 23:04:53 -0200

    transforming in pu

--- src/app/synchronous-machine/synchronous-machine.component.ts
+++ src/app/synchronous-machine/synchronous-machine.component.ts
@@ -22,6 +22,7 @@ export class SynchronousMachineComponent implements OnInit {
     changeIa;
     changeFatPot;
     changeType;
+    updatePu;
     // var to print
     vt = 120;
     iara;
@@ -35,6 +36,8 @@ export class SynchronousMachineComponent implements OnInit {
     private fatPot: boolean = true;
     private isGerador: boolean = false;
     private subexcitado: boolean = false;
+    public ra: Number = 0.33;
+    public xs: Number = 4.1;
 
     constructor() {
         function changeFp($event){
@@ -43,7 +46,7 @@ export class SynchronousMachineComponent implements OnInit {
           console.log('Changing fp to ' + value);
 
           this.fp0 = format(value);
-          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador);
+          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador, this.ra, this.xs);
         }
 
         function changeIa($event){
@@ -52,21 +55,26 @@ export class SynchronousMachineComponent implements OnInit {
           console.log('Changing Ia Percent to ' + value);
 
           this.iaPercent = format(value);
-          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador);
+          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador, this.ra, this.xs);
         }
 
         function changeFatPot(){
           console.log('Changing FatPot to ' + this.fatPot);
-          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador);
+          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador, this.ra, this.xs);
         }
         function changeType(){
           console.log('Changing Type to ' + this.fatPot);
-          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador);
+          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador, this.ra, this.xs);
+        }
+        function updatePu(){
+          console.log('update Pu values');
+          this.updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador, this.ra, this.xs);
         }
         this.changeFp = changeFp;
         this.changeIa = changeIa;
         this.changeFatPot = changeFatPot;
         this.changeType = changeType;
+        this.updatePu = updatePu;
       }
 
       ngOnInit() {
@@ -90,132 +98,143 @@ export class SynchronousMachineComponent implements OnInit {
           return math.multiply(math.complex( 0 , xs), ia);
         }
 
-          console.log(calcEf(10, 2, 3, 7));
+        console.log(calcEf(10, 2, 3, 7));
 
-          console.log('Creating SVG');
-          let w = 450;
-          let h = 300;
+        console.log('Creating SVG');
+        let w = 450;
+        let h = 300;
 
-          let offsetX = 57;
-          let offsetY = 122;
+        let offsetX = 57;
+        let offsetY = 122;
 
-          let svg = d3.select('#pl-diagram')
-            .attr('width', w)
-            .attr('height', h);
+        let svg = d3.select('#pl-diagram')
+          .attr('width', w)
+          .attr('height', h);
 
-          // Vt
-          svg.append('line')
-            .classed('vector', true)
-            .classed('vectorVt', true)
-            .attr('id', 'vt');
+        // Vt
+        svg.append('line')
+          .classed('vector', true)
+          .classed('vectorVt', true)
+          .attr('id', 'vt');
 
-          // Ia*Ra
-          svg.append('line')
-            .classed('vector', true)
-            .classed('vectorIaRa', true)
-            .attr('id', 'iara');
+        // Ia*Ra
+        svg.append('line')
+          .classed('vector', true)
+          .classed('vectorIaRa', true)
+          .attr('id', 'iara');
 
-          // Ia*j*Xs
-          svg.append('line')
-            .classed('vector', true)
-            .classed('vectorIajXs', true)
-            .attr('id', 'iajxs');
+        // Ia*j*Xs
+        svg.append('line')
+          .classed('vector', true)
+          .classed('vectorIajXs', true)
+          .attr('id', 'iajxs');
 
-          // Ef
-          svg.append('line')
-            .classed('vector', true)
-            .classed('vectorEf', true)
-            .attr('id', 'ef');
+        // Ef
+        svg.append('line')
+          .classed('vector', true)
+          .classed('vectorEf', true)
+          .attr('id', 'ef');
 
-          // ia
-          svg.append('line')
-            .classed('vector', true)
-            .classed('vectorIa', true)
-            .attr('id', 'ia');
+        // ia
+        svg.append('line')
+          .classed('vector', true)
+          .classed('vectorIa', true)
+          .attr('id', 'ia');
 
-          function updateDiagram(iaPercent, fatPot, fp0, isGerador){
+        function updateDiagram(iaPercent, fatPot, fp0, isGerador, rapu, xspu){
 
-            // example 6.3 page 307
-            let vt, iax, iay, xs, ra;
-            let efx, efy, ia, kva, ia0, vt0;
-            let fp, phi, raia, raiax, raiay;
-            let iajxs, iajxsx, iajxsy;
+          // example 6.3 page 307
+          let vt, iax, iay, xs, ra;
+          let efx, efy, ia, kva, ia0, vt0;
+          let fp, phi, raia, raiax, raiay;
+          let iajxs, iajxsx, iajxsy;
+          let vb, sb, zb, ib, d;
 
-            vt0 = 208;
-            kva = 5000;
-            ia0 = 5000 / ( Math.sqrt(3) * vt0);
+          vt0 = 208;
+          kva = 5000;
+          ia0 = 5000 / ( Math.sqrt(3) * vt0);
 
-            phi = Math.acos(fp0);
-            if (isGerador) ia0 = -ia0;
-            iax = ia0 * iaPercent * Math.cos(phi) / 100;
-            iay = ia0 * iaPercent * Math.sin(phi) / 100; //@TODO check validation FP negative or positive
-            if (fatPot === true) {
-              iay = ia0 * -1 * iaPercent * Math.sin(phi) / 100;
-            }
-            ia = math.complex(iax, iay);
-            console.log('ia', ia)
-            vt = 120; // vt/Math.sqrt(3)
-            xs = 8;
-            ra = 1;
-            console.log('aki');
-            let d;
-            d = calcEf(vt, xs, ia, ra);
-            console.log('\nef',d.abs(), d.arg());
 
-            efx = calcEf(vt, xs, ia, ra).re;
-            efy = calcEf(vt, xs, ia, ra).im;
-            raia = calcRaIa(vt, xs, ia, ra);
-            raiax = calcRaIa(vt, xs, ia, ra).re;
-            raiay = calcRaIa(vt, xs, ia, ra).im;
+          vb = 208;
+          sb = kva;
+          zb = vb*vb/sb;
+          ib = sb / (vb * Math.sqrt(3));
 
-            iajxs = calcIaJXs(vt, xs, ia, ra);
-            iajxsx = calcIaJXs(vt, xs, ia, ra).re;
-            iajxsy = calcIaJXs(vt, xs, ia, ra).im;
+          console.log(zb, '\n\n')
 
-            console.log(iajxsx, raiax)
-            // Dados Vt, Ia, If => descobrir Ef, If
-
-            // offset IA
-            let offsetIa = 5;
-            updateVector('ia', [0, 0, offsetIa * iax, offsetIa * iay]);
-            updateVector('vt', [0, 0, vt, 0]);
-            updateVector('iara', [vt, 0, raiax, raiay]);
-            updateVector('iajxs', [raiax + vt, raiay, iajxsx, iajxsy]);
-            updateVector('ef', [0, 0, efx, efy]);
-            console.log(vt, '\nvt');
-            updateStatus(
-              math.complex(vt, 0),
-              math.complex(raiax, raiay),
-              math.complex(iajxsx, iajxsy),
-              math.complex(efx, efy),
-              math.complex(iax, iay),
-              fp0);
+          ra = rapu * zb;
+          xs = xspu * zb;
+          phi = Math.acos(fp0);
+          if (isGerador) ia0 = -ia0;
+          iax = ia0 * iaPercent * Math.cos(phi) / 100;
+          iay = ia0 * iaPercent * Math.sin(phi) / 100; //@TODO check validation FP negative or positive
+          if (fatPot === true) {
+            iay = ia0 * -1 * iaPercent * Math.sin(phi) / 100;
           }
+          ia = math.complex(iax, iay);
+          console.log('ia', ia)
+          vt = 120; // vt/Math.sqrt(3)
 
-          function updateVector(id, vector){
-            d3.select('#' + id)
-              .attr('x1', offsetX + vector[0])
-              .attr('y1', h - (offsetY + vector[1]))
-              .attr('x2', offsetX + vector[2] + vector[0])
-              .attr('y2', h - (offsetY + vector[3] + vector[1]));
-          }
+          efx = calcEf(vt, xs, ia, ra).re;
+          efy = calcEf(vt, xs, ia, ra).im;
+          raia = calcRaIa(vt, xs, ia, ra);
+          raiax = calcRaIa(vt, xs, ia, ra).re;
+          raiay = calcRaIa(vt, xs, ia, ra).im;
 
-          let updateStatus = (vt, iara, iajxs, ef, ia, fp) => {
-            let format = (e) => math.format(e, {notation: 'fixed', precision: 2});
-            let polar = (e) => format(math.abs(e)) + ' ∟ ' +format(math.arg(e) / 2 / Math.PI * 360) + '°';
+          iajxs = calcIaJXs(vt, xs, ia, ra);
+          iajxsx = calcIaJXs(vt, xs, ia, ra).re;
+          iajxsy = calcIaJXs(vt, xs, ia, ra).im;
 
-            this.vt = format(vt);
-            this.iara =  polar(iara);
-            this.iajxs = polar(iajxs);
-            this.ef =  polar(ef);
-            this.ia =  polar(ia);
-            this.fp =  polar(fp);
-            this.subexcitado = vt.toPolar().r > ef.toPolar().r;
-          };
+          console.log(iajxsx, raiax)
+          // Dados Vt, Ia, If => descobrir Ef, If
 
-          updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador);
-          this.updateDiagram = updateDiagram;
-      }
+          // offset IA
+          let offsetIa = 8.35;
+          updateVector('ia', [0, 0, offsetIa * iax, offsetIa * iay]);
+          updateVector('vt', [0, 0, vt, 0]);
+          updateVector('iara', [vt, 0, raiax, raiay]);
+          updateVector('iajxs', [raiax + vt, raiay, iajxsx, iajxsy]);
+          updateVector('ef', [0, 0, efx, efy]);
+          console.log(vt, '\nvt');
+          updateStatus(
+            math.complex(vt/(zb*ib), 0),
+            math.complex(raiax/(zb*ib), raiay/(zb*ib)),
+            math.complex(iajxsx/(zb*ib), iajxsy/(zb*ib)),
+            math.complex(efx/(zb*ib), efy/(zb*ib)),
+            math.complex(iax/ib, iay/ib),
+            fp0);
+          // updateStatus(
+          //   math.complex(vt, 0),
+          //   math.complex(raiax, raiay),
+          //   math.complex(iajxsx, iajxsy),
+          //   math.complex(efx, efy),
+          //   math.complex(iax, iay),
+          //   fp0);
+        }
 
+        function updateVector(id, vector){
+          d3.select('#' + id)
+            .attr('x1', offsetX + vector[0])
+            .attr('y1', h - (offsetY + vector[1]))
+            .attr('x2', offsetX + vector[2] + vector[0])
+            .attr('y2', h - (offsetY + vector[3] + vector[1]));
+        }
+
+        let updateStatus = (vt, iara, iajxs, ef, ia, fp) => {
+          let format = (e) => math.format(e, {notation: 'fixed', precision: 2});
+          let polar = (e) => format(math.abs(e)) + ' ∟ ' +format(math.arg(e) / 2 / Math.PI * 360) + '°';
+
+          this.vt = format(vt);
+          this.iara =  polar(iara);
+          this.iajxs = polar(iajxs);
+          this.ef =  polar(ef);
+          this.ia =  polar(ia);
+          this.fp =  polar(fp);
+          this.subexcitado = vt.toPolar().r > ef.toPolar().r;
+        };
+
+        updateDiagram(this.iaPercent, this.fatPot, this.fp0, this.isGerador, this.ra, this.xs);
+        this.updateDiagram = updateDiagram;
+    }
 
   }
